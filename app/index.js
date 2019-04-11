3a5f016d9d8aa0ba0e12c9d179ef2fbd143a30cb
Jason Sheng <shengminming2011@gmail.com>
2019-04-11 13:52:20 -0400

    demonsrate package - import ui-toolkit - import button - what if button is one dep of ui-toolkit

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import {button} from '@jsh/button'
+
+
+const App = () => {
+  return (
+    <div>
+      <p>React here!</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,11 +1,12 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin'),
-    MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const HtmlWebPackPlugin = require('html-webpack-plugin'),
+//     MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 let path = require('path');
 
 module.exports = {
     entry: {
-        app: path.resolve(__dirname, '../src/app/index.js')
+        app: path.resolve(__dirname, '../app/index.js')
+        // app: 'src/index.js'
     },
     output: {
         publicPath: '/'
@@ -14,53 +15,53 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                include: [
-                    path.resolve(__dirname, '../src'),
-                    path.resolve(__dirname, '../../jsh-toolkit/src')
-                ],
+                // include: [
+                //     path.resolve(__dirname, '../src'),
+                //     path.resolve(__dirname, '../../jsh-toolkit/src')
+                // ],
                 use: {
                     loader: 'babel-loader'
                 }
             },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-loader',
-                        options: {minimize: false}
-                    }
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
+            // {
+            //     test: /\.html$/,
+            //     use: [
+            //         {
+            //             loader: 'html-loader',
+            //             options: {minimize: false}
+            //         }
+            //     ]
+            // },
+            // {
+            //     test: /\.scss$/,
+            //     use: [
+            //         MiniCssExtractPlugin.loader,
+            //         'css-loader',
+            //         'sass-loader'
+            //     ]
+            // }
         ]
     },
     plugins: [
-        new HtmlWebPackPlugin({
-            template: path.resolve(__dirname, '../src/app/index.html'),
-            filename: './index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css'
-        })
+        // new HtmlWebPackPlugin({
+        //     template: path.resolve(__dirname, '../src/app/index.html'),
+        //     filename: './index.html'
+        // }),
+        // new MiniCssExtractPlugin({
+        //     filename: '[name].css',
+        //     chunkFilename: '[id].css'
+        // })
     ],
-    resolve: {
-        alias: {
-            Common: path.resolve(__dirname, '../src/common'),
-            Const: path.resolve(__dirname, '../src/const'),
-            App: path.resolve(__dirname, '../src/app'),
-            Service: path.resolve(__dirname, '../src/service'),
-            View: path.resolve(__dirname, '../src/view'),
-            Form: path.resolve(__dirname, '../src/common/forms')
-        }
-    },
+    // resolve: {
+    //     alias: {
+    //         Common: path.resolve(__dirname, '../src/common'),
+    //         Const: path.resolve(__dirname, '../src/const'),
+    //         App: path.resolve(__dirname, '../src/app'),
+    //         Service: path.resolve(__dirname, '../src/service'),
+    //         View: path.resolve(__dirname, '../src/view'),
+    //         Form: path.resolve(__dirname, '../src/common/forms')
+    //     }
+    // },
     devServer: {
         historyApiFallback: true,
         disableHostCheck: true,
